667a4feb47c0092b6ef21dc5bd7bf9bc2652edc1
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2023-01-12 14:23:37 +0700

    fix: disable auth

--- project/school/src/main/kotlin/com/edu/school/infrastructure/security/SecurityConfig.kt
+++ project/school/src/main/kotlin/com/edu/school/infrastructure/security/SecurityConfig.kt
@@ -1,18 +1,36 @@
 package com.edu.school.infrastructure.security
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter
 
+@Configuration
 @EnableWebFluxSecurity
 class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http.authorizeExchange()
-            .anyExchange().authenticated()
-            .and().formLogin()
-            .and().build();
+//        TODO: Disable auth
+        return http
+            .csrf().disable()
+            .headers { headers ->
+                headers
+                    //Not allow to load the page in any iFrame or any site
+                    .frameOptions { frameOptions ->
+                        frameOptions.mode(XFrameOptionsServerHttpHeadersWriter.Mode.DENY)
+                    }
+                    //Allow scripting source on itself only
+                    .contentSecurityPolicy { contentSecurityPolicy ->
+                        contentSecurityPolicy.policyDirectives("script-src 'self'")
+                    }
+            }
+            .build();
     }
 }
+
+//.authorizeExchange()
+//            .anyExchange().authenticated()
+//            .and()
