e40d4de422c416dae43b07dab4394e5251429ce2
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2023-01-18 21:14:51 +0700

    feat: enable mapping LocalDate

--- project/school/src/main/kotlin/com/edu/school/infrastructure/framework/json/JacksonConfiguration.kt
+++ project/school/src/main/kotlin/com/edu/school/infrastructure/framework/json/JacksonConfiguration.kt
@@ -1,6 +1,7 @@
 package com.edu.school.infrastructure.framework.json
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,6 +12,7 @@ class JacksonConfiguration {
     fun jsonMapper(): JsonMapper {
         return JsonMapperImpl(
             jacksonObject = ObjectMapper()
+                .registerModule(JavaTimeModule())
         )
     }
 }
