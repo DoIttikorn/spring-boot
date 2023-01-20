77cd60bb41ffccb3df9f203b8f085e5a1de65651
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2023-01-20 08:35:50 +0700

    feat: update queryAllTeacher could not status = D

--- project/school/src/main/kotlin/com/edu/school/domain/teacher/repository/TeacherRepositoryImpl.kt
+++ project/school/src/main/kotlin/com/edu/school/domain/teacher/repository/TeacherRepositoryImpl.kt
@@ -3,9 +3,11 @@ package com.edu.school.domain.teacher.repository
 import com.edu.school.domain.teacher.model.Teacher
 import com.edu.school.infrastructure.database.repository.RepositoryFramework
 import org.springframework.context.annotation.Primary
+import org.springframework.data.domain.Sort
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Primary
@@ -14,6 +16,9 @@ interface TeacherRepositoryImpl : TeacherRepository, RepositoryFramework<Teacher
 
     @Query("SELECT * FROM teacher WHERE id = :id")
     override fun findById(id: Long): Mono<Teacher>
+
+    @Query("SELECT * FROM teacher WHERE status != 'D' ORDER BY id ASC  ")
+    override fun findAll(sort: Sort): Flux<Teacher>
 
     @Query("UPDATE teacher  SET status = 'D' WHERE id = :id")
     override fun deleteById(
