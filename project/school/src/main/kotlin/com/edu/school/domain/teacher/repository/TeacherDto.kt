edaa4bdd75a3d5f5e14694561373a2a065a63015
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2023-01-31 09:33:56 +0700

    feat: change connect db to dto file

---
+++ project/school/src/main/kotlin/com/edu/school/domain/teacher/repository/TeacherDto.kt
@@ -0,0 +1,23 @@
+package com.edu.school.domain.teacher.repository
+
+import com.edu.school.domain.teacher.model.Teacher
+import com.edu.school.infrastructure.database.repository.RepositoryFramework
+import org.springframework.data.domain.Sort
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.query.Param
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+interface TeacherDto : RepositoryFramework<Teacher, Long> {
+
+    @Query("SELECT * FROM teacher WHERE id = :id")
+    override fun findById(id: Long): Mono<Teacher>
+
+    @Query("SELECT * FROM teacher WHERE status != 'D' ORDER BY id ASC  ")
+    override fun findAll(sort: Sort): Flux<Teacher>
+
+    @Query("UPDATE teacher  SET status = 'D' WHERE id = :id")
+    override fun deleteById(
+        @Param("id") id: Long,
+    ): Mono<Void>
+}
--- project/school/src/main/kotlin/com/edu/school/domain/teacher/repository/TeacherRepositoryImpl.kt
+++ project/school/src/main/kotlin/com/edu/school/domain/teacher/repository/TeacherRepositoryImpl.kt
@@ -1,27 +1,32 @@
 package com.edu.school.domain.teacher.repository
 
 import com.edu.school.domain.teacher.model.Teacher
-import com.edu.school.infrastructure.database.repository.RepositoryFramework
 import org.springframework.context.annotation.Primary
 import org.springframework.data.domain.Sort
-import org.springframework.data.r2dbc.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Primary
 @Repository
-interface TeacherRepositoryImpl : TeacherRepository, RepositoryFramework<Teacher, Long> {
+class TeacherRepositoryImpl(
+    private val database: TeacherDto,
+) : TeacherRepository {
 
-    @Query("SELECT * FROM teacher WHERE id = :id")
-    override fun findById(id: Long): Mono<Teacher>
 
-    @Query("SELECT * FROM teacher WHERE status != 'D' ORDER BY id ASC  ")
-    override fun findAll(sort: Sort): Flux<Teacher>
+    override fun save(teacher: Teacher): Mono<Teacher> {
+        return database.save(teacher)
+    }
 
-    @Query("UPDATE teacher  SET status = 'D' WHERE id = :id")
-    override fun deleteById(
-        @Param("id") id: Long,
-    ): Mono<Void>
+    override fun findById(id: Long): Mono<Teacher> {
+        return database.findById(id)
+    }
+
+    override fun findAll(sort: Sort): Flux<Teacher> {
+        return database.findAll(sort)
+    }
+
+    override fun deleteById(id: Long): Mono<Void> {
+        return database.deleteById(id)
+    }
 }
